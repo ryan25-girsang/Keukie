87ed0a7e9cb4611c4be0d3e298eabf8a0b66bd75
ryan25-girsang1 <ryangirsang25@gmail.com>
2026-07-11 00:51:25 +0700

    perbaiki baseUrl

--- keukie_app/lib/config/api_config.dart
+++ keukie_app/lib/config/api_config.dart
@@ -1,3 +1,11 @@
+import 'dart:io';
+
 class ApiConfig {
-  static const String baseUrl = 'http//10.0.2.2:8000/api';
+  static String get baseUrl {
+    if (Platform.isAndroid) {
+      return 'http:/10.0.2.2:8000/api';
+    } else {
+      return 'http:/127.0.1:8000/api';
+    }
+  }
 }
